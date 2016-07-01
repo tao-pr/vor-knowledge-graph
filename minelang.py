af418c7600a3231637203675cbdb96fc1ef162a1
Tao PR <pataoengineer@gmail.com>
2016-07-01 14:39:38 +0700

    Minelang : Renamed from minekb.py

---
+++ minelang.py
@@ -0,0 +1,23 @@
+"""
+Language miner
+---
+The program explores the annotated text set, 
+creates a set of intent classifiers and keyword taggers.
+---
+@author Tao PR (github/starcolon)
+"""
+
+from pylib.text import structure as TextStructure
+from pylib.text import texthasher as TextHash
+from pylib.knowledge.graph import Knowledge
+
+if __name__ == '__main__':
+  # TAOTODO: Implement the sequence of process
+
+  # Reads in the raw annotated sentence list
+
+  # Train intent classifiers
+
+  # Train keyword taggers
+
+  pass
