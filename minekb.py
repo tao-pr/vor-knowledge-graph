200de772dd6d99d0eac7bc56556a30cbce8a0bab
Tao PR <pataoengineer@gmail.com>
2016-07-01 14:28:28 +0700

    Package : Make pylib a package

---
+++ minekb.py
@@ -0,0 +1,11 @@
+"""
+Knowledge miner script
+@author Tao PR (github/starcolon)
+"""
+
+from pylib.text import structure as TS
+from pylib.text import texthasher as TH
+from pylib.knowledge.graph import Knowledge
+
+if __name__ == '__main__':
+  pass
--- pylib/text/structure.py
+++ pylib/text/structure.py
@@ -11,7 +11,6 @@ import json
 from termcolor import colored
 from textblob import TextBlob
 
-
 # TAOTODO:
 """
 Extract part of speech structure of the given text
@@ -25,9 +24,29 @@ def pos_tag(texts):
 """
 Extract a keyword from the given list of words
 with regards to the specified intent.
+@param {object} tagger model
 @param {list} of tokenised text
-@param {string} intent
 @return {string} of keyword
 """
-def extract_keywords(texts,intent):
+def extract_keyword(tagger_model,texts):
+  pos = pos_tag(texts)
+  if len(pos)==0:
+    return None
+  else:
+    # Tag the position of the keyword in the POS components
+    keyword_pos = tagger_model.tag(pos)
+    # Extract the keyword from the matched POS position, if any
+    matched = [w for (p,w) in pos if p==keyword_pos]
+    if len(matched)>0:
+      return matched[0]
+    else:
+      return None
+
+"""
+Train the keyword tagger model from the annotated training set
+@param {list} of Y : annotated POS as a keyword
+@param {list} of X : raw text
+"""
+def train_keyword_tagger(labels,dataset):
+  # TAOTODO: HMM-based training
   pass
