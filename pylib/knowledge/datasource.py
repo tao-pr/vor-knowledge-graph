d1ffdaebb7fa2356f22392c970f290caa73e2499
starcolon <tao@starcolon.com>
2016-07-01 14:59:30 +0700

    Knowledge : Add initial datasource

---
+++ pylib/knowledge/datasource.py
@@ -0,0 +1,24 @@
+"""
+Mining data source access
+@author Tao PR (github.com/starcolon)
+"""
+from pymongo import MongoClient
+from pymongo import InsertOne
+
+class MineDB:
+
+  def __init__(self,host,db,coll):
+    addr       = "mongodb://{0}:27017/".format(host)
+    self.mongo = MongoClient(addr)
+    self.src   = self.mongo[coll]
+
+  def query(self,conditions):
+    return self.src.find(conditions)
+
+  def insert(self,record):
+    new_record = InsertOne(record)
+    self.src.bulk_write([new_record])
+
+  def insert_many(self,records):
+    new_records = [InsertOne(r) for r in records]
+    self.src.bulk_write(new_records)
