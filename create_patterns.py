cd62dc278f2563d36c8fa7da998a741a62bdb138
Tao PR <pataoengineer@gmail.com>
2016-07-05 01:06:56 +0700

    Rename create_trainset.py -> create_patterns.py

--- create_patterns.py
+++ create_patterns.py
@@ -15,7 +15,7 @@ arguments = argparse.ArgumentParser()
 arguments.add_argument('--verbose', dest='verbose', action='store_true', help='Turn verbose output on.')
 arguments.add_argument('--db', type=str, default='vor', help='Specify the database name to store input.')
 arguments.add_argument('--col', type=str, default='text', help='Specify the collection name to store input.')
-arguments.add_argument('--input', type=str, default=None, help='Specify an input CSV file path for file mode.') 
+# arguments.add_argument('--input', type=str, default=None, help='Specify an input CSV file path for file mode.') 
 args = vars(arguments.parse_args(sys.argv[1:]))
 
 def cli_input(dbsrc):
@@ -28,16 +28,16 @@ def cli_input(dbsrc):
   add_to_db(dbsrc, sentence, who, how, where, when)
   print(colored("[ADDED]","green"))
 
-def file_input(dbsrc,path,verbose):
-  with open(path, 'rb') as csvfile:
-    io = csv.reader(csvfile, delimeter=',')
-    for row in io:
-      sentence,who,how,where,when = row
-      who   = None if len(who)==0 else who
-      how   = None if len(how)==0 else how
-      where = None if len(where)==0 else where
-      when  = None if len(when)==0 else when
-      add_to_db(dbsrc, sentence, who, how, where, when)
+# def file_input(dbsrc,path,verbose):
+#   with open(path, 'rb') as csvfile:
+#     io = csv.reader(csvfile, delimeter=',')
+#     for row in io:
+#       sentence,who,how,where,when = row
+#       who   = None if len(who)==0 else who
+#       how   = None if len(how)==0 else how
+#       where = None if len(where)==0 else where
+#       when  = None if len(when)==0 else when
+#       add_to_db(dbsrc, sentence, who, how, where, when)
 
 def add_to_db(db,sentence,who,how,where,when):
   db.insert({
@@ -55,11 +55,11 @@ if __name__ == '__main__':
   verbose         = args['verbose']
   mine_src        = MineDB('localhost',db_name,collection_name)
 
-  if args['input'] is not None:
-    # Input CSV file mode
-    print(colored('[Reading CSV input] : ','magenta') + args['input'])
-    file_input(mine_src,args['input'],verbose)
-  else:
+  # if args['input'] is not None:
+  #   # Input CSV file mode
+  #   print(colored('[Reading CSV input] : ','magenta') + args['input'])
+  #   file_input(mine_src,args['input'],verbose)
+  # else:
     # Input CLI mode
     while True:
       cli_input(mine_src)
