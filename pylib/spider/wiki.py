1e5eba4d8e45144fc81754750296dc6d0e417e8c
starcolon <tao@starcolon.com>
2016-07-11 16:17:32 +0700

    Test : Add nosetest for crawler module

--- pylib/spider/wiki.py
+++ pylib/spider/wiki.py
@@ -4,8 +4,14 @@ Wikipedia page crawler and scraper
 
 from . import crawler
 
-def download_wiki(url):
-  pass
+def download_wiki(url,verbose=False):
+  selector = [
+    ('title', wiki_title),
+    ('contents', wiki_contents),
+    ('rels', wiki_rels)
+  ]
+  content  = crawler.download_page(url,selector,verbose)
+  return content
 
 def wiki_title(page):
   return page.find('h1#firstHeading').text()
@@ -15,7 +21,12 @@ Scrape the main article content from the specified
 wikipedia page
 """
 def wiki_contents(page):
-  return [] # TAOTODO:
+  contents   = []
+  paragraphs = page.find('#bodyContent p')
+  for p in paragraphs:
+    contents.append(p.text())
+
+  return contents
 
 """
 Scrape all related links (to other wiki pages)
@@ -26,6 +37,6 @@ def wiki_rels(page):
   for li in page.find('ul li'):
     link = li.find('a')
     # The item must locate a link to another wiki page
-    links.push(link.attr('href'))
+    links.append(link.attr('href'))
 
   return links
