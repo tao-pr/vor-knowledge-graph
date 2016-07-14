239cd3eb70e206cf03384c6d82131e9f364ba5b7
Tao PR <pataoengineer@gmail.com>
2016-07-14 16:32:13 +0700

    Annotate : (WIP) Draft individual sentence annotation process

--- annotate.py
+++ annotate.py
@@ -11,6 +11,7 @@ import argparse
 from termcolor import colored
 from pylib.text import structure as TextStructure
 from pylib.knowledge.datasource import MineDB
+from nltk.tokenize.punkt import PunktSentenceTokenizer
 
 arguments = argparse.ArgumentParser()
 arguments.add_argument('--verbose', dest='verbose', action='store_true', help='Turn verbose output on.')
@@ -36,16 +37,30 @@ and generate each of the sentence from the topic.
 """
 def raw_records(crawl_collection):
 
+  # Prepare a naive sentence tokeniser utility
+  pst = PunktSentenceTokenizer()
+
   # The unannotated wiki page will not have
   # attribute "annotated"
   for rec in crawl_collection.query({'downloaded': False, '$exists':{'annotated' : False}}):
-    yield rec
+    _id     = rec['_id']
+    content = rec['content']['contents']
+    # A wiki page may probably comprise of multiple content
+    for c in content:
+      # Explode a long topic into list of sentences
+      sentences = pst.sentences_from_text(c)
+      for s in sentences:
+        yield (_id,s)
 
 """
 Prompt the user to annotate the given text sentence
 """
-def cli_annotate(ann_collection,text):
-  pass # TAOTODO:
+def cli_annotate(crawl_collection,ann_collection):
+  def annotate(_id,text):
+    # Annotate and mark the wiki ID as annotated
+    pass # TAOTODO:
+
+  return annotate
 
 if __name__ == '__main__':
   # Prepare a connection to the crawled dataset
@@ -53,5 +68,8 @@ if __name__ == '__main__':
   crawl_collection = init_crawl_collection()
   ann_collection   = init_annotate_collection()
 
+  # Make an annotator function
+  annotate = cli_annotate(crawl_collection, ann_collection)
+
   # Iterate through each unannotated sentence
-  [cli_annotate(t,ann_collection) for t in raw_records(crawl_collection)]
+  [annotate(_id,t) for (_id,t) in raw_records(crawl_collection)]
