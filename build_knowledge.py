93dd9cd59154acf20c8cee6afbfdf084c926d3f8
Tao PR <pataoengineer@gmail.com>
2016-08-04 11:26:23 +0700

    build_knowledge : (WIP) Fix POS tagging mechanism

--- build_knowledge.py
+++ build_knowledge.py
@@ -42,52 +42,50 @@ def iter_topic(crawl_collection,start):
       continue
     m = 0
     content = wiki['content']
-    print(colored('[Extracting wiki] : ','cyan'), content['title'])
+    
+    if args['verbose']:
+      print(colored('[Extracting wiki] : ','cyan'), content['title'])
+    
     # A wiki page may probably comprise of multiple content
-    for c in content:
+    for c in content['contents']:
       # Explode a long topic into list of sentences
       sentences = pst.sentences_from_text(c)
       for s in sentences:
         m += 1
-        yield (content['title'],s)
+        yield (content['title'],s.split(' '))
 
     # TAOTODO: After all sentences are processed,
     # mark the current wiki record as 'processed'
 
     n += 1
-    print(content['title'] + " processed with {0} nodes.".format(m))
-    print(colored("{0} wiki documents processed so far...".format(n),'blue'))
+    if args['verbose']:
+      print(content['title'] + " processed with {0} nodes.".format(m))
+      print(colored("{0} wiki documents processed so far...".format(n),'blue'))
 
 
 if __name__ == '__main__':
 
   # Initialise a knowledge database
-  if args['verbose']:
-    print(colored('Initialising knowledge graph database...','cyan'))
+  print(colored('Initialising knowledge graph database...','cyan'))
   kb = Knowledge('localhost','vor','root',args['root'])
 
   # Load existing pos patterns
-  if args['verbose']:
-    print(colored('Loading POS patterns...','cyan'))
+  print(colored('Loading POS patterns...','cyan'))
   patterns = PatternCapture()
   patterns.load('./pos-patterns')
 
   # Initialise a crawling dataset connection
-  if args['verbose']:
-    print(colored('Initialising wikipedia crawling collection...'))
+  print(colored('Initialising wikipedia crawling collection...'))
   crawl_collection = init_crawl_collection()
 
   # Iterate through the crawling database
-  if args['verbose']:
-    print(colored('Starting...','magenta'))
   for topic,sentence in iter_topic(crawl_collection,args['start']):
     # Break the sentence into knowledge nodes
     pos      = TextStructure.pos_tag(sentence)
     kb_nodes = patterns.capture(pos)
 
-    # TAODEBUG:
-    print(pos)
-    print(kb_nodes)
+    if args['verbose']:
+      print(kb_nodes)
 
     # Create a set of knowledge links
     # for node in kb_nodes:
