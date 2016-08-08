db8b3a72c04951ad3c27e696ba114b7e0887fcaa
Tao PR <pataoengineer@gmail.com>
2016-08-08 15:39:21 +0700

    visualise : Add initial knowledge.js

---
+++ jslib/knowledge.js
@@ -0,0 +1,30 @@
+/**
+ * Knowledge graph query
+ * @author TaoPR (github.com/starcolon)
+ */
+
+var Knw = {}
+var orient = require('orientdb-js');
+var Promise = require('bluebird');
+
+/**
+ * Make a connection to the OrientDB
+ * @param {string} database name
+ * @param {string} username
+ * @param {string} password
+ */
+Knw.connect = function(db,usrname,psw){
+  // Do nothing if connected
+  if (Knw.__db)
+    return;
+
+  Knw.__db = orient.connect({
+    'database': db,
+    'user':     usrname,
+    'psw':      psw
+  })
+}
+
+
+
+module.exports = Knw;
