86db868fa33f304db3a14fefa542e394d78ebb4b
starcolon <tao@starcolon.com>
2016-08-08 15:58:59 +0700

    visualise : Add initial package.json

--- visualise.js
+++ visualise.js
@@ -3,6 +3,19 @@
  @author TaoPR (github.com/starcolon)
  */
 
- var KB = require('./jslib/knowledge.js');
+var args = process.argv.slice(2);
+var KB   = require('./jslib/knowledge.js');
+
+
+// Validate the arguments initially
+(function validateArgs(args){
+  if (args.length==0){
+    console.error('[ERROR] You need to supply OrientDB root database like:');
+    console.error('$ node visualise MYPASSWORD');
+    process.exit(1);
+  }
+})(args);
+
+
 
  
