8b06cc52cd3077560931923c0dc633735a9032ec
starcolon <tao@starcolon.com>
2017-02-18 15:42:40 +0700

    analyse : added

---
+++ analyse.py
@@ -0,0 +1,20 @@
+"""
+Study the distribution and relations between words
+@author TaoPR (github.com/starcolon)
+"""
+
+import json
+import os.path
+import pyorient
+import numpy as np
+from termcolor import colored
+from pyorient.exceptions import PyOrientSchemaException
+from pylib.knowledge.graph import Knowledge
+from pylib.knowledge.datasource import MineDB
+
+def load_graph(minedb):
+  pass
+
+def load_word2vec(model_path):
+  pass
+
