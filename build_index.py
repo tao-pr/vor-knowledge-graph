fa2d3dcb9500804d72af98e2156894b2995019b0
starcolon <tao@starcolon.com>
2017-02-27 00:19:49 +0700

    build_index : (WIP) load model + load kb

--- build_index.py
+++ build_index.py
@@ -11,5 +11,27 @@ from termcolor import colored
 arguments = argparse.ArgumentParser()
 arguments.add_argument('--verbose', dest='verbose', action='store_true', help='Turn verbose output on.')
 arguments.add_argument('--limit', type=int, default=100, help='Maximum number of topics we want to build index')
+arguments.add_argument('--modelpath', type=str, default='./models/word2vec.bin', help='Path of the word2vec binary model.')
 args = vars(arguments.parse_args(sys.argv[1:]))
 
+def create_index(kb, model):
+  pass
+
+def load_word2vec_model(path):
+  if not os.path.isfile(model_path):
+    print(colored('[ERROR] word2vec model does not exist.','red'))
+    raise RuntimeError('Model does not exist')
+  print(colored('[Model] loading binary model.','cyan'))
+  return word2vec.WordVectors.from_binary(model_path, encoding='ISO-8859-1')
+
+if __name__ == '__main__':
+  # Load word2vec model
+  model_path = os.path.realpath(args['modelpath'])
+  model = load_word2vec_model(model_path)
+
+  # Initialise a knowledge database
+  print(colored('Initialising knowledge graph database...','cyan'))
+  kb = Knowledge('localhost','vor','root',args['root'])
+  
+  # TAOTODO:
+  pass
