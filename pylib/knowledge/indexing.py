12526157e85e14877f92861aeddc07fd47601bf4
starcolon <tao@starcolon.com>
2017-03-01 01:38:52 +0700

    indexing : draft [IndexDomain]

--- pylib/knowledge/indexing.py
+++ pylib/knowledge/indexing.py
@@ -8,21 +8,19 @@ from termcolor import colored
 
 LOCAL_PREFIX  = 'http://localhost:8983/solr/'
 
-"""
-Initialise a connection to Solr
-"""
-def svr(url=LOCAL_PREFIX):
-  return pysolr.Solr(url, timeout=8)
+class IndexDomain:
+  def __init__(self, solr_svr_addr=LOCAL_PREFIX)
+    self.solr = pysolr.Solr(url, timeout=8)
 
-"""
-Create a new Solr index if does not exist,
-otherwise, load the existing
-"""
-def create_index(collection_name):
-  pass
+  """
+  Create a new Solr index if does not exist,
+  otherwise, load the existing
+  """
+  def create_index(collection_name):
+    pass
 
-def is_index_exist(collection_name):
-  pass
+  def is_index_exist(collection_name):
+    pass
 
-def add_to_index(solr,node):
-  pass
+  def add_to_index(solr,node):
+    pass
