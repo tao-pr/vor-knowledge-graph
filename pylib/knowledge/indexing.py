9bc706ec5529e24aa08ef75bf3c7a10fedf984f4
Tao PR <pataoengineer@gmail.com>
2017-02-28 22:50:45 +0700

    indexing : (WIP) initial commit

---
+++ pylib/knowledge/indexing.py
@@ -0,0 +1,20 @@
+"""
+Solr interface
+@author Tao PR (github.com/starcolon)
+"""
+
+import pysolr
+from termcolor import colored
+
+"""
+Create a new Solr index if does not exist,
+otherwise, load the existing
+"""
+def create_index(collection_name, where):
+  pass
+
+def is_index_exist(collection_name):
+  pass
+
+def add_to_index(solr,node):
+  pass
