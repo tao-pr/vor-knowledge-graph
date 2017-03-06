587d68a81bf6f94ef8e60530498237182df345d3
starcolon <tao@starcolon.com>
2017-03-06 23:48:52 +0700

    visualise : (WIP) FIX over-nested promises

--- visualise.js
+++ visualise.js
@@ -26,74 +26,72 @@ var indexGraphMapper = function(nodes){
 }
 
 var circularGraphMapper = function(nodes){
-  return new Promise((done,reject) => {
-    // Reduce the form of nodes so they become renderable.
-    var L = nodes.filter((n) => n['@class']=='TOPIC').length;
-    var degreeStep = 2*Math.PI/L;
-    var i = -1;
+  // Reduce the form of nodes so they become renderable.
+  var L = nodes.filter((n) => n['@class']=='TOPIC').length;
+  var degreeStep = 2*Math.PI/L;
+  var i = -1;
 
-    nodes = nodes.map((n) => {
-      i++;
-      var randomDegree = Math.random()*2*Math.PI;
-      var randomR = Math.random()*0.8;
-      var x = randomR * Math.cos(randomDegree);
-      var y = randomR * Math.sin(randomDegree);
+  nodes = nodes.map((n) => {
+    i++;
+    var randomDegree = Math.random()*2*Math.PI;
+    var randomR = Math.random()*0.8;
+    var x = randomR * Math.cos(randomDegree);
+    var y = randomR * Math.sin(randomDegree);
 
-      return {
-        id:    n['@rid'],
-        type:  n['@class'],
-        label: n['@class']=='TOPIC' ? n.title : n.w,
-        x:     n['@class']=='TOPIC' ? Math.cos(degreeStep*i) : x,
-        y:     n['@class']=='TOPIC' ? Math.sin(degreeStep*i) : y, 
-        size:  n['@class']=='TOPIC' ? 10 : 1,
-        color: n['@class']=='TOPIC' ? '#F00000' : '#FFAAAA'
-      }
-    })
+    return {
+      id:    n['@rid'],
+      type:  n['@class'],
+      label: n['@class']=='TOPIC' ? n.title : n.w,
+      x:     n['@class']=='TOPIC' ? Math.cos(degreeStep*i) : x,
+      y:     n['@class']=='TOPIC' ? Math.sin(degreeStep*i) : y, 
+      size:  n['@class']=='TOPIC' ? 10 : 1,
+      color: n['@class']=='TOPIC' ? '#F00000' : '#FFAAAA'
+    }
+  })
 
-    // Take outbound edges of those underlying nodes
-    console.log('Enumerating edges...');
-    var edges = [];
-    var collectEdges = (es) => es.then((e) => {
-      edges.push(e);
+  // Take outbound edges of those underlying nodes
+  console.log('Enumerating edges...');
+  var edges = [];
+  var collectEdges = (es) => es.then((e) => {
+    edges.push(e);
+  });
+  var topic = (n) => n.type=='TOPIC';
+
+  // Prepare edge enumuration jobs
+  var jobs = nodes
+    .filter(topic)
+    .map(KB.getOutE(100))
+    .map((n) => {
+      return collectEdges(n);
     });
-    var topic = (n) => n.type=='TOPIC';
 
-    // Prepare edge enumuration jobs
-    var jobs = nodes
-      .filter(topic)
-      .map(KB.getOutE(100))
-      .map((n) => {
-        return collectEdges(n);
-      });
+  // Make sure edges of all underlying nodes are processed.
+  return Promise
+    .all(jobs).then(() => [nodes,edges])
+    .then((p) => {
 
-    // Make sure edges of all underlying nodes are processed.
-    return Promise
-      .all(jobs).then(() => [nodes,edges])
-      .then((p) => {
+      console.log('Transforming nodes & edges ...')
+      var nodes = p[0];
+      var edges = p[1];
 
-        console.log('Transforming nodes & edges ...')
-        var nodes = p[0];
-        var edges = p[1];
+      // Flatten edges
+      edges = edges.reduce((a,b) => a.concat(b), []);
 
-        // Flatten edges
-        edges = edges.reduce((a,b) => a.concat(b), []);
-
-        // Make all edges renderable
-        edges = edges.map((e) => {
-          return {
-            id:     Math.random()*10000,
-            source: e.out,
-            target: e.in,
-            type:   'curve'
-          }
-        })
-
-        var graph = { nodes: nodes, edges: edges }; 
-        var sgraph = JSON.stringify(graph);
-
-        return sgraph
+      // Make all edges renderable
+      edges = edges.map((e) => {
+        return {
+          id:     Math.random()*10000,
+          source: e.out,
+          target: e.in,
+          type:   'curve'
+        }
       })
-  }) 
+
+      var graph = { nodes: nodes, edges: edges }; 
+      var sgraph = JSON.stringify(graph);
+
+      return sgraph
+    })
 }
 
 function saveToJSON(outputPath){
