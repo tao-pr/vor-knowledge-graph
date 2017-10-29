b64a5108b451be0be980a190767c469dad053413
starcolon <tao@starcolon.com>
2017-10-29 11:55:53 +0700

    removal of unused code

--- pylib/text/cleanser.py
+++ pylib/text/cleanser.py
@@ -10,34 +10,4 @@ Remove unwanted tokens from the given text
 """
 def cleanse(txt):
   return ''.join(filter(str.isalnum, txt))
-#   for p in patterns():
-#     txt_ = re.sub(p, ' ', txt_)
-#   return txt_
-
-# def patterns():
-#   return [ \
-#     r'^\-',
-#     r'\"',\
-#     r"\'",\
-#     r'\:',\
-#     r'\;',\
-#     r'\#',\
-#     r'\^',\
-#     r'\(',\
-#     r'\)',\
-#     r'\,',\
-#     r'\{.+\}',\
-#     r'\{',\
-#     r'\}',\
-#     r'\\'\
-#     r'\<.*\>',\
-#     r'&#.+;',\
-#     r'\.',\
-#     r'\+',\
-#     r'\!',\
-#     r'\n',\
-#     r'[\d+]',
-#     r' \w{1,2}\.',
-#     r'\[.*\]',
-#     r'title=\"\w*\"']
 
