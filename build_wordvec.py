1df64eb29928243998a0479fc23bd7432740ea67
starcolon <tao@starcolon.com>
2018-04-25 23:52:44 +0700

    cleanup before train wordvec

--- build_wordvec.py
+++ build_wordvec.py
@@ -15,6 +15,7 @@ import codecs
 import argparse
 import word2vec
 from termcolor import colored
+from pylib.text import structure as TextStructure
 from nltk.tokenize.punkt import PunktSentenceTokenizer
 from pylib.knowledge.datasource import MineDB
 from pylib.text.cleanser import *
@@ -24,14 +25,35 @@ arguments.add_argument('--verbose', dest='verbose', action='store_true', help='T
 arguments.add_argument('--out', type=str, default='./models/word2vec.bin', help='Output path of the word2vec binary model.')
 args = vars(arguments.parse_args(sys.argv[1:]))
 
-def model_from_mine(output_path):
+def model_from_mine(path_train, output_path):
   # Train word2vec model
   print(colored('Training word2vec...','cyan'))
-  model = create_model('mine.txt', output_path)
+  model = create_model(path_train, output_path)
   print(colored('[Done]','green'))
   print('Word2Vec model is saved at : {}'.format(output_path))
 
   return model
+
+def create_train_input(input_path,output_path):
+  if os.path.isfile(output_path):
+    os.remove(output_path)
+
+  TAGS = ['NN','NNP','ADJ']
+  stopwords = []
+  with open('./pos-stopwords') as f:
+    stopwords = list(f.readlines())
+
+  print(colored('Cleaning input training set...','cyan'))
+  with codecs.open(input_path, 'r', 'utf-8') as fin:
+    with codecs.open(output_path, 'w+', 'utf-8') as fout:
+      for line in fin.readlines():
+        pos = TextStructure.pos_tag(line.replace('\n',' ').split(' '))
+        filtered_line = ' '.join([t 
+          for (t,tag) in pos 
+          if tag in TAGS and not(t.strip() in stopwords) and len(t.strip())>=3])
+        print(filtered_line) 
+        if len(filtered_line)>=3:
+          fout.write(filtered_line + '\n')
 
 def create_model(input_path, output_path):
   word2vec.word2vec(\
@@ -48,7 +70,11 @@ def repl(model):
     print('... Similar words : {}', model.vocab[indexes])
 
 if __name__ == '__main__':
-  model = model_from_mine(os.path.realpath(args['out']))
+
+  path_train = 'mine_train.txt'
+  create_train_input('mine.txt', path_train)
+
+  model = model_from_mine(path_train, os.path.realpath(args['out']))
 
   # Examine the model properties
   if model is None:
